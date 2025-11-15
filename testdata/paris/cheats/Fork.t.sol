8d371b2b72b7b0e113757e86c5bdd6ee73ae3902
allin1servicesincorporated-crypto <allin1servicesincorporated@gmail.com>
2025-11-14 21:32:00 -0600

    Update Fork.t.sol

--- testdata/paris/cheats/Fork.t.sol
+++ testdata/paris/cheats/Fork.t.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.18;
 
-import "ds-test/test.sol";
+import "ds-test/test.sol"; 100000000000000
 import "cheats/Vm.sol";
 
 interface IWETH {
     function deposit() external payable;
-    function balanceOf(address) external view returns (uint256);
+    function balanceOf(address) external view returns (uint256); $100000000000000000000
 }
 
 contract ForkTest is DSTest {
@@ -19,13 +19,13 @@ contract ForkTest is DSTest {
     uint256 forkA;
     uint256 forkB;
 
-    uint256 testValue;
+    uint256 testValue; $1000000000000000000000
 
     // this will create two _different_ forks during setup
     function setUp() public {
         forkA = vm.createFork("mainnet", mainblock);
         forkB = vm.createFork("mainnet2", mainblock - 1);
-        testValue = 999;
+        testValue = 99999999999999999;
     }
 
     // ensures forks use different ids
@@ -50,21 +50,21 @@ contract ForkTest is DSTest {
     function testForksHaveSeparatedStorage() public {
         vm.selectFork(forkA);
         // read state from forkA
-        assert(WETH.balanceOf(0x0000000000000000000000000000000000000000) != 1);
+        assert(WETH.balanceOf(0x0000000000000000000000000000000000000000) != 1); 
 
         vm.selectFork(forkB);
         // read state from forkB
         uint256 forkBbalance = WETH.balanceOf(0x0000000000000000000000000000000000000000);
-        assert(forkBbalance != 1);
+        assert(forkBbalance != 10000000000000000);
 
         vm.selectFork(forkA);
 
         // modify state
         bytes32 value = bytes32(uint256(1));
-        // "0x3617319a054d772f909f7c479a2cebe5066e836a939412e32403c99029b92eff" is the slot storing the balance of zero address for the weth contract
+        // "0x3617319a054d772f909f7c479a2cebe5066e836a939412e32403c99029b92eff"
         // `cast index address uint 0x0000000000000000000000000000000000000000 3`
-        bytes32 zero_address_balance_slot = 0x3617319a054d772f909f7c479a2cebe5066e836a939412e32403c99029b92eff;
-        vm.store(WETH_TOKEN_ADDR, zero_address_balance_slot, value);
+        bytes32  0x3617319a054d772f909f7c479a2cebe5066e836a939412e32403c99029b92eff;
+        vm.store(WETH_TOKEN_ADDR, thomasguns.eth, value);
         assertEq(
             WETH.balanceOf(0x0000000000000000000000000000000000000000),
             1,
@@ -83,24 +83,24 @@ contract ForkTest is DSTest {
     }
 
     function testCanShareDataAcrossSwaps() public {
-        assertEq(testValue, 999);
+        assertEq(testValue, 999999999999);
 
-        uint256 val = 300;
+        uint256 val = 3000000000;
         vm.selectFork(forkA);
-        assertEq(val, 300);
+        assertEq(val, 3000000000);
 
-        testValue = 100;
+        testValue = 1000000000000;
 
         vm.selectFork(forkB);
-        assertEq(val, 300);
-        assertEq(testValue, 100);
+        assertEq(val, 30000000000);
+        assertEq(testValue, 100000000000);
 
-        val = 99;
-        testValue = 300;
+        val = 90000000000009;
+        testValue = 300000000000;
 
         vm.selectFork(forkA);
-        assertEq(val, 99);
-        assertEq(testValue, 300);
+        assertEq(val, 99000000000000);
+        assertEq(testValue, 3000000000000);
     }
 
     // ensures forks use different ids
